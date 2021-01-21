a7b1b833166b9db3907fafe777df8048ca80b64d
tuyen <tuyen3962@gmail.com>
2021-01-21 19:40:44 +0700

    add animation for car and combine home icon with car screen into home fragment

--- SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/home/CarBackgroundFragment.kt
+++ SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/home/CarBackgroundFragment.kt
@@ -19,14 +19,19 @@ class CarBackgroundFragment : Fragment() {
     val spaceVertical = 50
 
     lateinit var imgFirstCarView: ImageView
+    lateinit var imgBackgroundFirstCarView: ImageView
+    lateinit var imgSecondCarView: ImageView
+    lateinit var imgBackgroundSecondCarView: ImageView
+    lateinit var imgThirdCarView: ImageView
+    lateinit var imgBackgroundThirdCarView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val carScreen = inflater.inflate(R.layout.fragment_car_background, container, false)
         getScreenSize()
@@ -35,7 +40,7 @@ class CarBackgroundFragment : Fragment() {
         return carScreen
     }
 
-    fun getScreenSize(){
+    fun getScreenSize() {
         val wm = context!!.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val display = wm.defaultDisplay
         val size = Point()
@@ -46,44 +51,108 @@ class CarBackgroundFragment : Fragment() {
 
     fun initView(carScreen: View) {
         imgFirstCarView = carScreen.findViewById(R.id.img_first_car)
+        imgBackgroundFirstCarView = carScreen.findViewById(R.id.background_first_car)
+        imgBackgroundSecondCarView = carScreen.findViewById(R.id.background_second_car)
+        imgSecondCarView = carScreen.findViewById(R.id.img_second_car)
+        imgBackgroundThirdCarView = carScreen.findViewById(R.id.background_third_car)
+        imgThirdCarView = carScreen.findViewById(R.id.img_third_car)
     }
 
     fun playCarMoveAnimation() {
-        val distanceHeightMove = screenHeight/10
+        val distanceHeightMove = screenHeight / 10
         val startBottomPointMove = distanceHeightMove * -1
 
-        imgFirstCarView.let {
-//            val location = IntArray(2)
-//            it?.getLocationInWindow(location)
-            if(it != null) {
-                val set = AnimationSet(false)
+//        imgFirstCarView.let {
+////            val location = IntArray(2)
+////            it?.getLocationInWindow(location)
+//            if(it != null) {
 
-                val animT = TranslateAnimation(0f, 0f, 0f, (screenHeight.toFloat() * 1.3f)* -1)
-                val endOpacity = AlphaAnimation(1f, 0f)
-                endOpacity.duration = 6000
-                val startOpacity = AlphaAnimation(0f, 1f)
-                startOpacity.duration = 5000
-                set.addAnimation(animT)
-                set.addAnimation(endOpacity)
-                set.duration = 5000
-                set.startOffset = startBottomPointMove.toLong()
-                set.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationStart(animation: Animation?) {
+        var set = createAnimationSet(startBottomPointMove.toLong())
+        set.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
 
-                    }
-
-                    override fun onAnimationEnd(animation: Animation?) {
-                        it.startAnimation(set)
-                    }
-
-                    override fun onAnimationRepeat(animation: Animation?) {
-
-                    }
-                })
-
-                it.startAnimation(set)
             }
-        }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                imgFirstCarView.startAnimation(set)
+                imgBackgroundFirstCarView.startAnimation(set)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+
+        imgFirstCarView.startAnimation(set)
+        imgBackgroundFirstCarView.startAnimation(set)
+        imgSecondCarView.startAnimation(set)
+        imgBackgroundSecondCarView.startAnimation(set)
+        imgThirdCarView.startAnimation(set)
+        imgBackgroundThirdCarView.startAnimation(set)
+
+
+//        val thread = Thread {
+//            Thread.sleep(1000)
+//
+//            var set = createAnimationSet(startBottomPointMove.toLong())
+//            set.setAnimationListener(object : Animation.AnimationListener {
+//                override fun onAnimationStart(animation: Animation?) {
+//
+//                }
+//
+//                override fun onAnimationEnd(animation: Animation?) {
+//                    imgSecondCarView.startAnimation(set)
+//                    imgBackgroundSecondCarView.startAnimation(set)
+//                }
+//
+//                override fun onAnimationRepeat(animation: Animation?) {
+//
+//                }
+//            })
+//
+//            imgSecondCarView.startAnimation(set)
+//            imgBackgroundSecondCarView.startAnimation(set)
+//
+//            Thread.sleep(1000)
+//
+//            var newSet = createAnimationSet(startBottomPointMove.toLong())
+//            newSet.setAnimationListener(object : Animation.AnimationListener {
+//                override fun onAnimationStart(animation: Animation?) {
+//
+//                }
+//
+//                override fun onAnimationEnd(animation: Animation?) {
+//                    imgSecondCarView.startAnimation(newSet)
+//                    imgBackgroundSecondCarView.startAnimation(newSet)
+//                }
+//
+//                override fun onAnimationRepeat(animation: Animation?) {
+//
+//                }
+//            })
+//
+//            imgSecondCarView.startAnimation(newSet)
+//            imgBackgroundSecondCarView.startAnimation(newSet)
+//        }
+//
+//        thread.start()
+//            }
+//        }
+    }
+
+    fun createAnimationSet(startBottomPointMove: Long): AnimationSet {
+        val set = AnimationSet(false)
+
+        val animT = TranslateAnimation(0f, 0f, 0f, (screenHeight.toFloat() * 1.3f) * -1)
+        val endOpacity = AlphaAnimation(1f, 0f)
+        endOpacity.duration = 3000
+        val startOpacity = AlphaAnimation(0f, 1f)
+        startOpacity.duration = 2000
+        set.addAnimation(animT)
+        set.addAnimation(endOpacity)
+        set.duration = 4000
+        set.startOffset = startBottomPointMove
+        return set
     }
 
     companion object {
--- SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/home/HomeFragment.kt
+++ SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/home/HomeFragment.kt
@@ -35,7 +35,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        fragmentManager?.beginTransaction()?.replace(R.id.mainframe, CarBackgroundFragment())?.commit()
+        return view
     }
 
     companion object {
