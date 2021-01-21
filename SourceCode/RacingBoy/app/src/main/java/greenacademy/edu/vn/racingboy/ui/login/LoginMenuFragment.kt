1e83c9b7df59fb1f6adaaa7d719a514b62aaff70
tuyen <tuyen3962@gmail.com>
2021-01-21 19:52:54 +0700

    set onclick event to button register

--- SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/login/LoginMenuFragment.kt
+++ SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/login/LoginMenuFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import greenacademy.edu.vn.racingboy.R
+import greenacademy.edu.vn.racingboy.ui.home.HomeFragment
 
 class LoginMenuFragment : Fragment() {
 
@@ -19,6 +20,14 @@ class LoginMenuFragment : Fragment() {
         loginBtn.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()?.let {
                 it.add(R.id.fragment, LoginFragment(activity!!))
+                it.addToBackStack("LoginMenuFragment")
+                it.commit()
+            }
+        }
+        val registerButton = view.findViewById<Button>(R.id.btnToRegister)
+        registerButton.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()?.let {
+                it.add(R.id.fragment, HomeFragment())
                 it.addToBackStack("LoginMenuFragment")
                 it.commit()
             }
