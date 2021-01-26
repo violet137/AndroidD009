67846896b8ab6a180fb1f1d80a82a330358e833c
Nguyen Thanh Phong <v.phongnt18@vinid.net>
2021-01-26 19:36:37 +0700

    fix code

--- SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/login/LoginFragment.kt
+++ SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/login/LoginFragment.kt
@@ -15,6 +15,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import greenacademy.edu.vn.racingboy.R
+import greenacademy.edu.vn.racingboy.ui.home.HomeFragment
 
 
 class LoginFragment(val activity: Activity) : Fragment() {
@@ -23,8 +24,8 @@ class LoginFragment(val activity: Activity) : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
 
@@ -35,22 +36,30 @@ class LoginFragment(val activity: Activity) : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
+                .requestEmail()
+                .build()
         val mGoogleSignInClient = GoogleSignIn.getClient(activity, gso)
         val account = GoogleSignIn.getLastSignedInAccount(activity)
-val btnSignIn = view.findViewById<Button>(R.id.btnSignInGG)
-       btnSignIn.setOnClickListener {
-           if (account == null){
-               val signInIntent = mGoogleSignInClient.signInIntent
-               startActivityForResult(signInIntent, RC_SIGN_IN)
-               btnSignIn.text = "Log out"
-           }
+        val btnSignIn = view.findViewById<Button>(R.id.btnSignInGG)
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+        btnSignIn.setOnClickListener {
+            if (account == null) {
+                val signInIntent = mGoogleSignInClient.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+                btnSignIn.text = "Log out"
+            }
+        }
+        btnLogin.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fragment, HomeFragment())
+                commit()
+            }
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN){
+        if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
 
         }
--- SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/login/MainActivity.kt
+++ SourceCode/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/login/MainActivity.kt
@@ -4,27 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import greenacademy.edu.vn.racingboy.R
-<<<<<<< HEAD
 import greenacademy.edu.vn.racingboy.ui.buycar.BuyCarFragment
-=======
 import greenacademy.edu.vn.racingboy.ui.buyitem.BuyItemFragment
 import greenacademy.edu.vn.racingboy.ui.leaderboard.LeaderBoardFragment
 import greenacademy.edu.vn.racingboy.ui.leaderboard.model.LeaderBoardUser
 
 class MainActivity() : AppCompatActivity() {
->>>>>>> qy
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().apply {
-<<<<<<< HEAD
-            replace(R.id.fragment,BuyCarFragment())
-=======
-            replace(R.id.fragment, LeaderBoardFragment())
-//            replace(R.id.fragment, BuyItemFragment())
->>>>>>> qy
+            replace(R.id.fragment, LoginFragment(this@MainActivity))
             commit()
         }
     }
