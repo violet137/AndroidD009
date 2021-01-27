424c1881d6347ebdf78aad657efffdf607dc968f
ducphu1924 <76478994+ducphu1924@users.noreply.github.com>
2021-01-27 11:39:59 +0700

    no message

---
+++ SourceCode/RacingBoy/RacingBoy/app/src/main/java/greenacademy/edu/vn/racingboy/ui/buycar/DataBuyCar.kt
@@ -0,0 +1,12 @@
+package greenacademy.edu.vn.racingboy.ui.buycar
+
+class DataBuyCar(
+        val imageCar: Int = 0 ,
+      val nameCar: String?,
+      val speed: String?,
+      val handling: String?,
+      val acceleration: String?,
+      val monneyLetbuyCar: String? ,
+      val colorCar: String? = ""
+
+)
